e81111254d91cd9907b1b00dc3b29f70cf5840eb
gachez <briangach98@gmail.com>
2022-11-30 07:32:44 +0300

    use correct uri

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
     BASE_URL: 'https://sell3r.vercel.app',
-    API_URI: 'https://sell3r.herokuapp.com/'
+    API_URI: 'https://sell3r.herokuapp.com'
 }
 
 module.exports.Config = Config
